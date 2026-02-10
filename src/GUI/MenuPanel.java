558e7f7d4fe69a091782223a069dae3bef09bbd9
Mazen Elkashlan <mazenelkashlan34@gmail.com>
2026-02-10 23:30:23 +0200

    started gui

---
+++ src/GUI/MenuPanel.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.*;
+
+public class MenuPanel {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+}
